ea968713b0b5b5c4d9add4b6d1f455cad61b7648
chenyue404 <601450225@qq.com>
2022-05-08 10:02:38 +0800

    choose config

--- app/src/main/java/com/cy/schemelaunchpad/MainActivity.kt
+++ app/src/main/java/com/cy/schemelaunchpad/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.cy.schemelaunchpad
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.Window
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.net.toUri
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cy.fixtableview.FixTableView
 import com.cy.schemelaunchpad.json.JsonUtil
 import com.google.gson.JsonSyntaxException
@@ -47,6 +49,8 @@ class MainActivity : Activity() {
     private var currentConfigName = ""
     private var currentConfig: ConfigBean? = null
     private var isEditMode = false
+    private val chooseConfigDialogAdapter by lazy { ChooseConfigDialogAdapter() }
+    private val chooseConfigDialog by lazy { createChooseConfigDialog() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -90,6 +94,26 @@ class MainActivity : Activity() {
         editItemDialog.onSaveListener = { column, row, itemConfigBean ->
             currentConfig?.itemMap?.put(getItemConfigKey(column, row), itemConfigBean)
         }
+        btName.click { showChooseConfigDialog() }
+        chooseConfigDialogAdapter.actionListener =
+            object : ChooseConfigDialogAdapter.ActionListener {
+                override fun onSelected(title: String) {
+                    currentConfigName = title
+                    updateView()
+                    saveCurrent()
+                    chooseConfigDialog.cancel()
+                }
+
+                override fun onRemove(title: String) {
+                    allConfigMap.remove(title)
+                    saveCurrent()
+                    chooseConfigDialog.cancel()
+                }
+
+                override fun onEdit(title: String) {
+                    showRenameDialog(title)
+                }
+            }
         updateView()
     }
 
@@ -98,10 +122,12 @@ class MainActivity : Activity() {
         sp.edit()
             .putString(SP_KEY_CONFIG, JsonUtil.toJson(allConfigMap))
             .apply()
+        sp.edit().putString(SP_KEY_CURRENT, currentConfigName).apply()
 
     }
 
     private fun updateView() {
+        currentConfig = allConfigMap[currentConfigName]
         val tableConfig = currentConfig?.tableConfig ?: return
         val itemMap = currentConfig?.itemMap
         btName.text = currentConfigName
@@ -139,6 +165,8 @@ class MainActivity : Activity() {
             matchConstraintPercentWidth = tableConfig.width * 0.01f
             matchConstraintPercentHeight = tableConfig.height * 0.01f
         }
+        chooseConfigDialogAdapter.currenConfigName = currentConfigName
+        chooseConfigDialogAdapter.dataList = allConfigMap.map { it.key }
     }
 
     private fun readStorage() {
@@ -202,4 +230,120 @@ class MainActivity : Activity() {
 
     private fun View.getBtn() = findViewById<Button>(R.id.btTitle)
     private fun View.getTv() = findViewById<TextView>(R.id.tvEdit)
+
+    private fun createChooseConfigDialog(): AlertDialog {
+        val recyclerView = RecyclerView(this).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            layoutManager = LinearLayoutManager(context)
+            adapter = chooseConfigDialogAdapter
+        }
+        return AlertDialog.Builder(this)
+            .setTitle(R.string.choose_config)
+            .setView(recyclerView)
+            .setNeutralButton(R.string.add)
+            { dialog, which ->
+                showRenameDialog()
+            }
+            .create()
+    }
+
+    private class ChooseConfigDialogAdapter :
+        RecyclerView.Adapter<ChooseConfigDialogAdapter.ViewHolder>() {
+
+        interface ActionListener {
+            fun onSelected(title: String)
+            fun onRemove(title: String)
+            fun onEdit(title: String)
+        }
+
+        var dataList = listOf<String>()
+            set(value) {
+                field = value
+                notifyDataSetChanged()
+            }
+        var actionListener: ActionListener? = null
+        var currenConfigName: String? = null
+
+        class ViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
+            val tvTitle = rootView.findViewById<TextView>(R.id.tvTitle)
+            val ibRemove = rootView.findViewById<ImageButton>(R.id.ibRemove)
+            val ibEdit = rootView.findViewById<ImageButton>(R.id.ibEdit)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_config_list,
+                parent, false
+            )
+        )
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val title = dataList[position]
+            val isCurrent = currenConfigName == title
+            holder.tvTitle.apply {
+                text = title
+                typeface = if (isCurrent) Typeface.DEFAULT_BOLD
+                else Typeface.DEFAULT
+            }
+            holder.itemView.click {
+                actionListener?.onSelected(title)
+            }
+            holder.ibEdit.click {
+                actionListener?.onEdit(title)
+            }
+            holder.ibRemove.click {
+                actionListener?.onRemove(title)
+            }
+            holder.ibRemove.visible(!isCurrent)
+        }
+
+        override fun getItemCount() = dataList.size
+    }
+
+    private fun showChooseConfigDialog() {
+        chooseConfigDialog.show()
+        chooseConfigDialogAdapter.currenConfigName = currentConfigName
+        chooseConfigDialogAdapter.dataList = allConfigMap.map { it.key }
+    }
+
+    private fun showRenameDialog(oldName: String? = null) {
+        val editText = EditText(this).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            setText(oldName)
+            setSelection(oldName?.length ?: 0)
+        }
+        AlertDialog.Builder(this)
+            .setTitle(R.string.rename)
+            .setView(editText)
+            .setPositiveButton(android.R.string.ok) { dialog, which ->
+                val newName = editText.text.toString()
+                if (newName.isEmpty()) return@setPositiveButton
+                if (allConfigMap.containsKey(newName)) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.name_already_exits),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setPositiveButton
+                }
+                val oldConfigBean = allConfigMap[oldName] ?: ConfigBean()
+                allConfigMap.remove(oldName)
+                allConfigMap[newName] = oldConfigBean
+                currentConfigName = newName
+                dialog.cancel()
+                updateView()
+                saveCurrent()
+            }
+            .setNegativeButton(android.R.string.cancel) { dialog, which ->
+                dialog.cancel()
+            }
+            .create()
+            .show()
+    }
 }
--- app/src/main/java/com/cy/schemelaunchpad/ext.kt
+++ app/src/main/java/com/cy/schemelaunchpad/ext.kt
@@ -5,3 +5,11 @@ import android.view.View
 fun View.click(listener: View.OnClickListener) {
     setOnClickListener(listener)
 }
+
+fun View.visible(visible: Boolean = true) {
+    visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
